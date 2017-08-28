f4635f1bbd17765191786e118e0cf029b591c51f
Idan Reed <idanclay18554@gmail.com>
2017-08-28 11:58:04 -0500

    Initial commit Lab 1.

---
+++ 01-Intro-VS-GitHub/01-Intro-VS-GitHub/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_VS_GitHub
+{
+    class Program
+    {
+
+        // Welcomes the user, you need to add ", welcome to CIS501"
+        // so the output be for example: Hello Jorge, welcome to CIS501!
+        static void Main(string[] args)
+        {
+            Console.Write("Type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+
+
+            // retain command window till user presses Enter
+            Console.ReadLine();
+        }
+    }
+}
