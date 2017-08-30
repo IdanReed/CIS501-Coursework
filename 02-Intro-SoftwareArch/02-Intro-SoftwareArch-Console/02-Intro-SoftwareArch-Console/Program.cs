863ede091ee9b5e719e55ad791bd689613c8b9f0
Idan Reed <idanclay18554@gmail.com>
2017-08-30 12:02:07 -0500

    Finished Console Lab 2.

--- 02-Intro-SoftwareArch/02-Intro-SoftwareArch-Console/02-Intro-SoftwareArch-Console/Program.cs
+++ 02-Intro-SoftwareArch/02-Intro-SoftwareArch-Console/02-Intro-SoftwareArch-Console/Program.cs
@@ -12,17 +12,37 @@ namespace Intro_SoftwareArch_Console
         // echos the user's name and prints a randomly chosen int:
         static void Main(string[] args)
         {
-            Console.Write("Type your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + "!");
+            Console.Write("Guess an int, M, in range 0..10:  M = ");
+            string m = Console.ReadLine();
+            if (Convert.ToInt32(m) > 10)
+            {
+                Console.WriteLine("Out of range");
+                Console.ReadLine();
+                return;
+            }
+            int max = 10 - Convert.ToInt32(m);
+            
+            Console.WriteLine("I guess int, N, in range 0..{0}", max);
 
             // how to generate random numbers:
             Random r = new Random();
             int min = 0;
-            int max = 99;
+            max = 10-Convert.ToInt32(m);
             int n = r.Next(min, max + 1);
+            Console.Write(n);
+            Console.Write("now you type an int, P, such that M + N + P = 10:  P =" );
+            string p = Console.ReadLine();
+            int sum = Convert.ToInt32(m) + Convert.ToInt32(n) + Convert.ToInt32(p);
+            Console.WriteLine(sum);
 
-            Console.WriteLine("Here is a random generated number between {0} to {1}: {2}", min, max, n);
+            if (sum == 10)
+            {
+                Console.WriteLine("You win!");
+            }else
+            {
+                Console.WriteLine("You lose!");
+            }
+            
 
             // retain command window till user presses Enter
             Console.ReadLine();
