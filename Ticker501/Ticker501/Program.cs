fdd8851cb06d77132c37c4a6a22bb07283d5dac1
Idan Reed <idanclay18554@gmail.com>
2017-09-14 23:53:05 -0500

    Lab 5 done.

--- Ticker501/Ticker501/Program.cs
+++ Ticker501/Ticker501/Program.cs
@@ -12,6 +12,8 @@ namespace Ticker501
         
         static void Main(string[] args)
         {
+            List<Stock> stockList = new List<Stock>();
+            ReadTickerFile(stockList);
             double balance = 0;
 
             Portfolio[] porfolios = new Portfolio[3];
@@ -19,106 +21,131 @@ namespace Ticker501
             {
                 porfolios[i] = new Portfolio();
             }
-
-            
-            List<String> accountitems = new List<String>();
-            accountitems.Add("Deposit Funds");
-            accountitems.Add("Portfolios");
-            accountitems.Add("Simulate Price");
-            int accountSelection = menuSelect("Account Menu", accountitems);
-
             bool accountSelectionValid;
-            switch (accountSelection)
+            while (true)
             {
-                case 0://Back
-                    System.Environment.Exit(1);
-                    break;
-                case 1://Deposit
-                    do
-                    {
-                        Console.Clear();
-                        accountSelectionValid = true;
-                        try
-                        {
-                            Console.Write("Deposit amount>: ");
-                            double deposit = Convert.ToInt32(Console.ReadLine());
-                            balance += deposit;
-
-                            Console.Clear();
-                            Console.Write(">New balance is " + balance);
-                            Console.ReadLine();
-                        }
-                        catch (Exception e)
+                List<String> accountitems = new List<String>();
+                accountitems.Add("Deposit Funds");
+                accountitems.Add("Portfolios");
+                accountitems.Add("Simulate Price");
+                int accountSelection = MenuSelect("Account Menu", accountitems);
+                
+            
+                switch (accountSelection)//Account page
+                {
+                    case 0://Back
+                        System.Environment.Exit(1);
+                        break;
+                    case 1://Deposit
+                        do
                         {
                             Console.Clear();
-                            Console.Write(">Not valid");
-                            accountSelectionValid = false;
-                        }
-                    } while (!accountSelectionValid);
-                    break;
-                case 2://Portfolio
-                    List<String> portfolioItems = new List<String>();
-                    portfolioItems.Add("One");
-                    portfolioItems.Add("Two");
-                    portfolioItems.Add("Three");
-                    int portfolioSelection = menuSelect("Portfolios", portfolioItems);
+                            accountSelectionValid = true;
+                            try
+                            {
+                                Console.Write("Deposit amount>: ");
+                                double deposit = Convert.ToInt32(Console.ReadLine());
+                                balance += deposit;
 
-                    Portfolio selectedProtfolio = null;
-                    switch (portfolioSelection)
-                    {
-                        case 0:
-                            break;
-                        case 1:
-                            selectedProtfolio = porfolios[1];
-                            break;
-                        case 2:
-                            selectedProtfolio = porfolios[2];
-                            break;
-                        case 3:
-                            selectedProtfolio = porfolios[3];
-                            break;
-                    }
+                                Console.Clear();
+                                Console.Write(">New balance is " + balance);
+                                Console.ReadLine();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Clear();
+                                Console.Write(">Not valid");
+                                accountSelectionValid = false;
+                            }
+                        } while (!accountSelectionValid);
+                        break;
+                    case 2://Portfolio
+                        List<String> portfolioItems = new List<String>();
+                        portfolioItems.Add("One");
+                        portfolioItems.Add("Two");
+                        portfolioItems.Add("Three");
+                        int portfolioSelection = MenuSelect("Portfolios", portfolioItems);
 
-                    if(selectedProtfolio != null)
-                    {
-                        List<String> portfolioActionItems = new List<String>();
-                        portfolioActionItems.Add("Buy");
-                        portfolioActionItems.Add("Sell");
-                        portfolioActionItems.Add("Info");
-                        int portfolioActionSelection = menuSelect("Portfolios", portfolioActionItems);
-
+                        Portfolio selectedProtfolio = null;
                         switch (portfolioSelection)
                         {
                             case 0:
                                 break;
-                            case 1://Buy
-
+                            case 1:
+                                selectedProtfolio = porfolios[1];
                                 break;
-                            case 2://Sell
+                            case 2:
+                                selectedProtfolio = porfolios[2];
                                 break;
-                            case 3://Info
+                            case 3:
+                                selectedProtfolio = porfolios[3];
                                 break;
                         }
-                    }
-                    break;
-                case 3://Simulate Price
-                    break;
+
+                        if (selectedProtfolio != null)
+                        {
+                            List<String> portfolioActionItems = new List<String>();
+                            portfolioActionItems.Add("Buy");
+                            portfolioActionItems.Add("Sell");
+                            portfolioActionItems.Add("Info");
+                            int portfolioActionSelection = MenuSelect("Portfolios", portfolioActionItems);
+
+                            switch (portfolioSelection)
+                            {
+                                case 0:
+                                    break;
+                                case 1://Buy
+                                    Console.Clear();
+                                    PrintList(stockList);
+                                    Console.WriteLine();
+                                    string purchaseAbbv = Console.ReadLine();
+
+                                    break;
+                                case 2://Sell
+                                    break;
+                                case 3://Info
+                                    break;
+                            }
+                        }
+                        break;
+                    case 3://Simulate Price
+                        break;
+                }
             }
         }
 
-        static void readTickerFile(string path, List<Stock> stockList)
+        static void ReadTickerFile(List<Stock> stockList)
         {
-            String[] lines = System.IO.File.ReadAllLines(path);
-            foreach (string line in lines)
+            bool valid;
+            do
             {
-                string[] parts = line.Split('-');
-                Stock nextStock = new Stock(Convert.ToDouble(parts[2].Substring(1)), parts[0], parts[1]);
-                stockList.Add(nextStock);
-            }
+                valid = true;
+                Console.Clear();
+                Console.Write("Path for ticker file: ");
+                string path = Console.ReadLine();
+                try
+                {
+                    String[] lines = System.IO.File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split('-');
+                        Stock nextStock = new Stock(Convert.ToDouble(parts[2].Substring(1)), parts[0], parts[1]);
+                        stockList.Add(nextStock);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.Write("Not vaild file");
+                    Console.ReadLine();
+                    valid = false;
+                }
+
+            } while (!valid);
         }
 
 
-        static int menuSelect(string menuHeader, List<String> menuItems)
+        static int MenuSelect(string menuHeader, List<String> menuItems)
         {
             string border = "------------------------------------";
             
@@ -163,20 +190,33 @@ namespace Ticker501
             }
             return -1;
         }
-        static int printList(List<Stock> list)
+        static void PrintList(List<Stock> list)
         {
             int avgWidth = 0;
             int count = 0;
             foreach(Stock s in list)
             {
-                avgWidth += s.abbv.Length;
+                avgWidth += s.abbv.Length+s.price.ToString().Length;
                 count++;
             }
             avgWidth = (int) (((double) avgWidth) / ((double) count));
 
-            foreach (string s in list)
+            int curPos = 0;
+            foreach (Stock s in list)
             {
-                Console.WriteLine(s);
+                if(curPos>2)
+                {
+                    Console.WriteLine();
+                    curPos = 0;
+                }
+                string line = s.abbv + "-" + s.price;
+                int spaceCount = avgWidth - line.Length + 5;
+                Console.Write(line);
+                for(int i = 0; i < spaceCount; i++)
+                {
+                    Console.Write(" ");
+                }
+                curPos++;
             }
         }
     }
