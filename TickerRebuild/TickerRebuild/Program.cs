dddcc8c7fe1215b8214afc04541f925158f0be67
Idan Reed <idanclay18554@gmail.com>
2017-11-08 00:55:25 -0600

    Lab 13 donish

---
+++ TickerRebuild/TickerRebuild/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickerRebuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Account
+    {
+
+    }
+    class Portfolio
+    {
+
+    }
+    class Stock
+    {
+        string name;
+        double price;
+    }
+    abstract class Transaction
+    {
+        protected double _gainLossInfulence;
+        Portfolio owner;
+    }
+    class Fee : Transaction
+    {
+        double value;
+
+        Fee(double valueIn)
+        {
+            value = valueIn;
+        }
+
+        public double gainLossInfulence
+        {
+            get
+            {
+
+            }
+        }
+    }
+    class Buy : Transaction
+    {
+        Stock stock;
+        int quantity;
+
+        Buy(Stock stockIn, int quantityIn)
+        {
+            stock = stockIn;
+            quantity = quantityIn;
+        }
+    }
+    class Sell : Transaction
+    {
+        Stock stock;
+        int quantity;
+        Sell(Stock stockIn, int quantityIn)
+        {
+            stock = stockIn;
+            quantity = quantityIn;
+        }
+    }
+}
